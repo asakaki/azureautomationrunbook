6803c1823d1316a9d051ee67fd59e329bf1ef135
asakakibara <akito.sakakibara1@akkodis.co.jp>
2024-07-26 14:15:35 +0900

    feat: Add scripts to manage VM power state
    Adds PowerShell scripts to start and stop Azure virtual machines.

---
+++ Azure/Manage_VM_PowerState/Start_single_VirtualMachine.ps1
@@ -0,0 +1,31 @@
+﻿# 実行用パラメーター
+param (
+    [parameter(mandatory)][String]$VmName,
+    [parameter(mandatory)][String]$ResourceGroupName
+)
+
+# 事前準備
+$ErrorActionPreference = "Continue"
+
+# Azureにログイン
+$login = Connect-AzAccount -Identity
+if (!($login)) {
+    Write-Output "Azureへのログインに失敗しました。"
+    Write-Error -Message $Error[0] -ErrorAction "Stop"
+}
+
+# 仮想マシンが存在することを確認
+if (!(Get-AzVm -Name $VmName -ResourceGroupName $ResourceGroupName)) {
+    Write-Output "仮想マシン: ${VmName}が見つかりません。"
+    Write-Error -Message $Error[0] -ErrorAction "Stop"
+}
+
+# 仮想マシンを起動
+$result = Start-AzVm -Name $VmName -ResourceGroupName $ResourceGroupName
+if (!($result)) {
+    Write-Output "仮想マシン: ${VmName}の起動に失敗しました。"
+    Write-Error -Message $Error[0] -ErrorAction "Stop"
+}
+
+# Azureからログアウト
+Disconnect-AzAccount
---
+++ Azure/Manage_VM_PowerState/Stop_single_VirtualMachine.ps1
@@ -0,0 +1,30 @@
+﻿# 実行用パラメーター
+param (
+    [parameter(mandatory)][String]$VmName,
+    [parameter(mandatory)][String]$ResourceGroupName
+)
+
+# 事前準備
+$ErrorActionPreference = "Continue"
+
+$login = Connect-AzAccount -Identity
+if (!($login)) {
+    Write-Output "Azureへのログインに失敗しました。"
+    Write-Error -Message $Error[0] -ErrorAction "Stop"
+}
+
+# 仮想マシンが存在することを確認
+if (!(Get-AzVm -Name $VmName -ResourceGroupName $ResourceGroupName)) {
+    Write-Output "仮想マシン: ${VmName}が見つかりません。"
+    Write-Error -Message $Error[0] -ErrorAction "Stop"
+}
+
+# 仮想マシンを停止
+$result = Stop-AzVm -Name $VmName -ResourceGroupName $ResourceGroupName -Force
+if (!($result)) {
+    Write-Output "仮想マシン: ${VmName}の停止に失敗しました。"
+    Write-Error -Message $Error[0] -ErrorAction "Stop"
+}
+
+# Azureからログアウト
+Disconnect-AzAccount
